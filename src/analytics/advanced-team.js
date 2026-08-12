bf7a69ef7ae1d34caf820b6061038b240a499641
Marksheehan13 <124387661@umail.ucc.ie>
2026-08-12 15:59:33 +0100

    fix: correct advanced team stats object syntax

--- src/analytics/advanced-team.js
+++ src/analytics/advanced-team.js
@@ -1,2 +1,2 @@
 import { estimatePossessions, pointsPerPossession } from './possessions.js';
-export function teamAdvancedStats(team={}){const poss=estimatePossessions(team);return {possessions:poss,fgPct=team.fga?Number(team.fgm)/Number(team.fga):null,threePct:team.tpa?Number(team.tpm)/Number(team.tpa):null,ftPct:team.fta?Number(team.ftm)/Number(team.fta):null,ppp:pointsPerPossession(team.pts,poss)}}
+export function teamAdvancedStats(team={}){const poss=estimatePossessions(team);return {possessions:poss,fgPct:team.fga?Number(team.fgm)/Number(team.fga):null,threePct:team.tpa?Number(team.tpm)/Number(team.tpa):null,ftPct:team.fta?Number(team.ftm)/Number(team.fta):null,ppp:pointsPerPossession(team.pts,poss)}}
